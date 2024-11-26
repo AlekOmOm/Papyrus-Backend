1316c69022004fce8f4aaae816b7af26c6877025
AlekOmOm <AlekOmOm@users.noreply.github.com>
2024-11-26 22:03:53 +0100

    Init - Maven Spring Boot setup
    This commit includes the addition of Maven wrapper files for easier build management and configuration across environments. It also sets up a basic Spring Boot project structure, including essential dependencies, application properties, and an initial test class. The project is geared towards supporting web and data access functionalities with Spring Boot, JPA, and WebFlux.

---
+++ src/main/java/com/alek0m0m/papyrusbackend/PapyrusBackendApplication.java
@@ -0,0 +1,13 @@
+package com.alek0m0m.papyrusbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PapyrusBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PapyrusBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/alek0m0m/papyrusbackend/PapyrusBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alek0m0m.papyrusbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PapyrusBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
