a955c36449f55724e485b7ad7a9896913c5bba53
AlekOmOm <AlekOmOm@users.noreply.github.com>
2024-11-27 11:01:58 +0100

    Update UserController path, add resetAutoIncrement method.
    Refactored `UserController` to use `@RequestMapping` for the "/user" path. Introduced `resetAutoIncrement` method in `UserRepository` for resetting the auto-increment value in the User table. Also, corrected syntax in `application.properties` for environment variable usage.

--- src/main/java/com/alek0m0m/papyrusbackend/user/UserController.java
+++ src/main/java/com/alek0m0m/papyrusbackend/user/UserController.java
@@ -2,9 +2,11 @@ package com.alek0m0m.papyrusbackend.user;
 
 import com.Alek0m0m.library.spring.web.mvc.BaseRESTController;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/user")
+@RestController
+@RequestMapping("/user")
 public class UserController extends BaseRESTController<UserDTOInput, UserDTO, User, UserMapper, UserService, UserRepository> {
 
     @Autowired
--- src/main/java/com/alek0m0m/papyrusbackend/user/UserRepository.java
+++ src/main/java/com/alek0m0m/papyrusbackend/user/UserRepository.java
@@ -1,9 +1,13 @@
 package com.alek0m0m.papyrusbackend.user;
 
 import com.Alek0m0m.library.spring.web.mvc.BaseRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends BaseRepository<User> {
-
+    @Modifying
+    @Query(value = "ALTER TABLE User AUTO_INCREMENT = 1", nativeQuery = true)
+    void resetAutoIncrement();
 }
