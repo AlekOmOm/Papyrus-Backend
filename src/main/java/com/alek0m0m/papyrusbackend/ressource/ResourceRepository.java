f1ae67cfe8ad4df7504ec8cedbef970e5aafd88f
AlekOmOm <AlekOmOm@users.noreply.github.com>
2024-12-03 21:27:52 +0100

    Fix resource table name and update URL mapping in controllers.
    Add the @Repository annotation to ResourceRepository and correct the SQL table name from "Ressource" to "resource" in the resetAutoIncrement method. Update ResourceController to use the correct URL mapping by changing the request path from "/users" to "/api/resources".

--- src/main/java/com/alek0m0m/papyrusbackend/ressource/ResourceRepository.java
+++ src/main/java/com/alek0m0m/papyrusbackend/ressource/ResourceRepository.java
@@ -3,9 +3,11 @@ package com.alek0m0m.papyrusbackend.ressource;
 import com.Alek0m0m.library.spring.web.mvc.BaseRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ResourceRepository extends BaseRepository<Resource> {
     @Modifying
-    @Query(value = "ALTER TABLE Ressource AUTO_INCREMENT = 1", nativeQuery = true)
+    @Query(value = "ALTER TABLE resource AUTO_INCREMENT = 1", nativeQuery = true)
     void resetAutoIncrement();
 }
