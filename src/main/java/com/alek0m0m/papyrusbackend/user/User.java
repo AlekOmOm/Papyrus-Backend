fa7ec8391265d33c349ad8a493348dd6b2317a65
Magnus Munk Braunstein <147395199+MagnusMunkBraunstein@users.noreply.github.com>
2024-12-04 10:53:25 +0100

    @OnetoOne anootation Init data done

--- src/main/java/com/alek0m0m/papyrusbackend/user/User.java
+++ src/main/java/com/alek0m0m/papyrusbackend/user/User.java
@@ -1,10 +1,12 @@
 package com.alek0m0m.papyrusbackend.user;
 
 import com.Alek0m0m.library.jpa.*;
+import com.alek0m0m.papyrusbackend.field.Field;
 import com.alek0m0m.papyrusbackend.ressource.Resource;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.Entity;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToOne;
 import lombok.*;
 
 import java.util.ArrayList;
@@ -25,6 +27,9 @@ public class User extends BaseEntity {
     @ManyToMany(mappedBy = "users")
     @JsonIgnore
     private List<Resource> ressources = new ArrayList<>();
+
+    @OneToOne(mappedBy = "user")
+    private Field field;
 
 
     // Setters (returning User)
